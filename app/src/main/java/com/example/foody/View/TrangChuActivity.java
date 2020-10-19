9db1fdf71dc7beceb2388df48c2f24ffec83aa70
lethekhoi <lethekhoi@gmail.com>
2020-10-19 13:41:55 +0700

    đăng xuất tài khoản

--- app/src/main/java/com/example/foody/View/TrangChuActivity.java
+++ app/src/main/java/com/example/foody/View/TrangChuActivity.java
@@ -1,26 +1,40 @@
 package com.example.foody.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import com.example.foody.Adapter.AdapterViewPagerTrangChu;
 import com.example.foody.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class TrangChuActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener, RadioGroup.OnCheckedChangeListener {
     ViewPager viewPagerTrangChu;
     RadioButton rbAnGi, rbODau;
     RadioGroup rbGroup;
+    Menu menu;
+    Toolbar toolbar;
+    FirebaseAuth auth;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trang_chu);
         Anhxa();
+        sharedPreferences = getSharedPreferences("luudangnhap", MODE_PRIVATE);
+        auth = FirebaseAuth.getInstance();
         AdapterViewPagerTrangChu adapterViewPagerTrangChu = new AdapterViewPagerTrangChu(getSupportFragmentManager());
         viewPagerTrangChu.setAdapter(adapterViewPagerTrangChu);
         viewPagerTrangChu.addOnPageChangeListener(this);
@@ -28,6 +42,8 @@ public class TrangChuActivity extends AppCompatActivity implements ViewPager.OnP
     }
 
     private void Anhxa() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         viewPagerTrangChu = findViewById(R.id.viewPagerTrangChu);
         rbAnGi = findViewById(R.id.rb_angi);
         rbODau = findViewById(R.id.rb_odau);
@@ -67,5 +83,28 @@ public class TrangChuActivity extends AppCompatActivity implements ViewPager.OnP
                 viewPagerTrangChu.setCurrentItem(1);
                 break;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_trang_chu, menu);
+        this.menu = menu;
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.itDangXuat:
+                auth.signOut();
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("mauser", "");
+                editor.commit();
+                Intent iDangNhap = new Intent(TrangChuActivity.this, DangNhapActivity.class);
+                startActivity(iDangNhap);
+                break;
+        }
+        return true;
     }
 }
